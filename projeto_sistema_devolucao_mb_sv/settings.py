4217257aad3213b2bae9209ff2442f4d0ef2b896
Matheus-Ap1 <matheusapmb@gmail.com>
2026-09-08 02:51:06 -0300

    update

--- projeto_sistema_devolucao_mb_sv/settings.py
+++ projeto_sistema_devolucao_mb_sv/settings.py
@@ -149,9 +149,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+# * [EXPLICAÇÃO] → Configurado para português brasileiro e fuso de São Paulo
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
